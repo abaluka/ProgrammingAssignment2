98d25cf793042305e3948a68a4e9c0cbc8006589
abaluka <l337abaluka@yahoo.com>
2015-03-21 12:14:10 -0500

    updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,12 @@
-# The makeCachMatrix and cacheSolve functions work together to calculate the inverse of a square mattix and store it in a cache, so that it may be retrieved rather than re-calculated if it has already been calculated
+# The makeCachMatrix and cacheSolve functions work together to calculate the
+#inverse of a square mattix and store it in a cache, so that it may be retrieved
+#rather than re-calculated if it has already been calculated
 # NB: These functions only work for SQUARE matrices
 
 
-# The makeCacheMatrix function creates a list containing functions that sets the value of a matrix, gets the value of that matrix, sets the inverse of that matrix, and gets the inverse of that matrix 
+# The makeCacheMatrix function creates a list containing functions that sets the
+#value of a matrix, gets the value of that matrix, sets the inverse of that
+#matrix, and gets the inverse of that matrix 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -18,7 +22,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-# The cacheSolve function checks to see if the inverse of the matrix defined in the makeCacheMatrix function has already been calculated. If so, it retrieves it from the cache. If not, it calculates it and stores it in the cache.
+# The cacheSolve function checks to see if the inverse of the matrix defined in
+#the makeCacheMatrix function has already been calculated. If so, it retrieves it
+#from the cache. If not, it calculates it and stores it in the cache.
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
         if(!is.null(m)) {
